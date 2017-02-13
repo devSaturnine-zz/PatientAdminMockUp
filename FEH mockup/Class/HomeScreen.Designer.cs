d54c996ee43ee6644bafc728af5e6620dbd62347
neil.dolley <neil.dolley@gmail.com>
2017-02-14 00:51:45 +0200

    Added Main Menu tiles

--- FEH mockup/Class/HomeScreen.Designer.cs
+++ FEH mockup/Class/HomeScreen.Designer.cs
@@ -28,18 +28,256 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeScreen));
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroTile2 = new MetroFramework.Controls.MetroTile();
+            this.metroTile3 = new MetroFramework.Controls.MetroTile();
+            this.metroTile4 = new MetroFramework.Controls.MetroTile();
+            this.metroTile5 = new MetroFramework.Controls.MetroTile();
+            this.metroTile6 = new MetroFramework.Controls.MetroTile();
+            this.label1 = new System.Windows.Forms.Label();
+            this.metroTile7 = new MetroFramework.Controls.MetroTile();
+            this.metroTile8 = new MetroFramework.Controls.MetroTile();
+            this.metroTile9 = new MetroFramework.Controls.MetroTile();
+            this.metroTile10 = new MetroFramework.Controls.MetroTile();
+            this.metroTile11 = new MetroFramework.Controls.MetroTile();
+            this.metroTile12 = new MetroFramework.Controls.MetroTile();
+            this.metroLabel9 = new MetroFramework.Controls.MetroLabel();
             this.SuspendLayout();
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.metroTile1.Location = new System.Drawing.Point(225, 227);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(175, 135);
+            this.metroTile1.TabIndex = 0;
+            this.metroTile1.Text = "Add Patient";
+            this.metroTile1.TileImage = ((System.Drawing.Image)(resources.GetObject("metroTile1.TileImage")));
+            this.metroTile1.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile1.UseSelectable = true;
+            this.metroTile1.UseStyleColors = true;
+            this.metroTile1.UseTileImage = true;
+            // 
+            // metroTile2
+            // 
+            this.metroTile2.ActiveControl = null;
+            this.metroTile2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.metroTile2.Location = new System.Drawing.Point(406, 227);
+            this.metroTile2.Name = "metroTile2";
+            this.metroTile2.Size = new System.Drawing.Size(175, 135);
+            this.metroTile2.TabIndex = 1;
+            this.metroTile2.Text = "View Patients";
+            this.metroTile2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.metroTile2.TileImage = ((System.Drawing.Image)(resources.GetObject("metroTile2.TileImage")));
+            this.metroTile2.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile2.UseSelectable = true;
+            this.metroTile2.UseStyleColors = true;
+            this.metroTile2.UseTileImage = true;
+            // 
+            // metroTile3
+            // 
+            this.metroTile3.ActiveControl = null;
+            this.metroTile3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.metroTile3.Location = new System.Drawing.Point(587, 227);
+            this.metroTile3.Name = "metroTile3";
+            this.metroTile3.Size = new System.Drawing.Size(175, 135);
+            this.metroTile3.TabIndex = 2;
+            this.metroTile3.Text = "Transfer Patient";
+            this.metroTile3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.metroTile3.TileImage = ((System.Drawing.Image)(resources.GetObject("metroTile3.TileImage")));
+            this.metroTile3.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile3.UseSelectable = true;
+            this.metroTile3.UseStyleColors = true;
+            this.metroTile3.UseTileImage = true;
+            // 
+            // metroTile4
+            // 
+            this.metroTile4.ActiveControl = null;
+            this.metroTile4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.metroTile4.Location = new System.Drawing.Point(225, 368);
+            this.metroTile4.Name = "metroTile4";
+            this.metroTile4.Size = new System.Drawing.Size(356, 135);
+            this.metroTile4.TabIndex = 3;
+            this.metroTile4.Text = "Reports";
+            this.metroTile4.TileImage = ((System.Drawing.Image)(resources.GetObject("metroTile4.TileImage")));
+            this.metroTile4.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile4.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            this.metroTile4.UseSelectable = true;
+            this.metroTile4.UseStyleColors = true;
+            this.metroTile4.UseTileImage = true;
+            // 
+            // metroTile5
+            // 
+            this.metroTile5.ActiveControl = null;
+            this.metroTile5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.metroTile5.Location = new System.Drawing.Point(587, 368);
+            this.metroTile5.Name = "metroTile5";
+            this.metroTile5.Size = new System.Drawing.Size(175, 135);
+            this.metroTile5.TabIndex = 4;
+            this.metroTile5.Text = "Psychologists";
+            this.metroTile5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.metroTile5.TileImage = ((System.Drawing.Image)(resources.GetObject("metroTile5.TileImage")));
+            this.metroTile5.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile5.UseSelectable = true;
+            this.metroTile5.UseStyleColors = true;
+            this.metroTile5.UseTileImage = true;
+            // 
+            // metroTile6
+            // 
+            this.metroTile6.ActiveControl = null;
+            this.metroTile6.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.metroTile6.Location = new System.Drawing.Point(225, 512);
+            this.metroTile6.Name = "metroTile6";
+            this.metroTile6.Size = new System.Drawing.Size(537, 65);
+            this.metroTile6.TabIndex = 5;
+            this.metroTile6.Text = "Profile";
+            this.metroTile6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.metroTile6.TileImage = ((System.Drawing.Image)(resources.GetObject("metroTile6.TileImage")));
+            this.metroTile6.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile6.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            this.metroTile6.UseSelectable = true;
+            this.metroTile6.UseStyleColors = true;
+            this.metroTile6.UseTileImage = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Light", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(132, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 65);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Welcome, Neil";
+            // 
+            // metroTile7
+            // 
+            this.metroTile7.ActiveControl = null;
+            this.metroTile7.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.metroTile7.Location = new System.Drawing.Point(1236, 442);
+            this.metroTile7.Name = "metroTile7";
+            this.metroTile7.Size = new System.Drawing.Size(172, 135);
+            this.metroTile7.Style = MetroFramework.MetroColorStyle.Brown;
+            this.metroTile7.TabIndex = 13;
+            this.metroTile7.Text = "Profile";
+            this.metroTile7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile7.UseSelectable = true;
+            // 
+            // metroTile8
+            // 
+            this.metroTile8.ActiveControl = null;
+            this.metroTile8.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.metroTile8.Location = new System.Drawing.Point(871, 227);
+            this.metroTile8.Name = "metroTile8";
+            this.metroTile8.Size = new System.Drawing.Size(175, 135);
+            this.metroTile8.Style = MetroFramework.MetroColorStyle.Orange;
+            this.metroTile8.TabIndex = 12;
+            this.metroTile8.Text = "dummy1";
+            this.metroTile8.UseSelectable = true;
+            // 
+            // metroTile9
+            // 
+            this.metroTile9.ActiveControl = null;
+            this.metroTile9.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.metroTile9.Location = new System.Drawing.Point(1052, 227);
+            this.metroTile9.Name = "metroTile9";
+            this.metroTile9.Size = new System.Drawing.Size(356, 135);
+            this.metroTile9.Style = MetroFramework.MetroColorStyle.Teal;
+            this.metroTile9.TabIndex = 11;
+            this.metroTile9.Text = "dummy2";
+            this.metroTile9.UseSelectable = true;
+            // 
+            // metroTile10
+            // 
+            this.metroTile10.ActiveControl = null;
+            this.metroTile10.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.metroTile10.Location = new System.Drawing.Point(874, 368);
+            this.metroTile10.Name = "metroTile10";
+            this.metroTile10.Size = new System.Drawing.Size(534, 65);
+            this.metroTile10.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroTile10.TabIndex = 10;
+            this.metroTile10.Text = "dummy3";
+            this.metroTile10.UseSelectable = true;
+            // 
+            // metroTile11
+            // 
+            this.metroTile11.ActiveControl = null;
+            this.metroTile11.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.metroTile11.Location = new System.Drawing.Point(1055, 442);
+            this.metroTile11.Name = "metroTile11";
+            this.metroTile11.Size = new System.Drawing.Size(175, 135);
+            this.metroTile11.Style = MetroFramework.MetroColorStyle.Silver;
+            this.metroTile11.TabIndex = 9;
+            this.metroTile11.Text = "metroTile11";
+            this.metroTile11.UseSelectable = true;
+            // 
+            // metroTile12
+            // 
+            this.metroTile12.ActiveControl = null;
+            this.metroTile12.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.metroTile12.BackColor = System.Drawing.Color.White;
+            this.metroTile12.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.metroTile12.Location = new System.Drawing.Point(874, 442);
+            this.metroTile12.Name = "metroTile12";
+            this.metroTile12.Size = new System.Drawing.Size(175, 135);
+            this.metroTile12.Style = MetroFramework.MetroColorStyle.Lime;
+            this.metroTile12.TabIndex = 8;
+            this.metroTile12.Text = "dummy4";
+            this.metroTile12.UseSelectable = true;
+            // 
+            // metroLabel9
+            // 
+            this.metroLabel9.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.metroLabel9.AutoSize = true;
+            this.metroLabel9.Location = new System.Drawing.Point(604, 699);
+            this.metroLabel9.Name = "metroLabel9";
+            this.metroLabel9.Size = new System.Drawing.Size(293, 19);
+            this.metroLabel9.TabIndex = 14;
+            this.metroLabel9.Text = "All Rights Reserved ©  2017 | Developed By: Neil";
             // 
             // HomeScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1203, 719);
+            this.ClientSize = new System.Drawing.Size(1500, 719);
+            this.Controls.Add(this.metroLabel9);
+            this.Controls.Add(this.metroTile7);
+            this.Controls.Add(this.metroTile8);
+            this.Controls.Add(this.metroTile9);
+            this.Controls.Add(this.metroTile10);
+            this.Controls.Add(this.metroTile11);
+            this.Controls.Add(this.metroTile12);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.metroTile6);
+            this.Controls.Add(this.metroTile5);
+            this.Controls.Add(this.metroTile4);
+            this.Controls.Add(this.metroTile3);
+            this.Controls.Add(this.metroTile2);
+            this.Controls.Add(this.metroTile1);
             this.Name = "HomeScreen";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroTile metroTile2;
+        private MetroFramework.Controls.MetroTile metroTile3;
+        private MetroFramework.Controls.MetroTile metroTile4;
+        private MetroFramework.Controls.MetroTile metroTile5;
+        private MetroFramework.Controls.MetroTile metroTile6;
+        private System.Windows.Forms.Label label1;
+        private MetroFramework.Controls.MetroTile metroTile7;
+        private MetroFramework.Controls.MetroTile metroTile8;
+        private MetroFramework.Controls.MetroTile metroTile9;
+        private MetroFramework.Controls.MetroTile metroTile10;
+        private MetroFramework.Controls.MetroTile metroTile11;
+        private MetroFramework.Controls.MetroTile metroTile12;
+        private MetroFramework.Controls.MetroLabel metroLabel9;
     }
 }
